c88f2771ee5409b70e02009f2da14e62ac2437bc
Xriss Xross <xriss_xross@outlook.com>
2022-02-18 09:44:24 +0000

    transferred code
    help, summon, checkqueue asnycs

--- main.py
+++ main.py
@@ -27,4 +27,41 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(
         type=discord.ActivityType.listening, name="everyone"))
 
+
+async def checkQueue(ctx):
+    if QUEUE != []:
+        QUEUE.pop(0)
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        voice_client = ctx.voice_client
+        info = ydl.extract_info(QUEUE[0][1], download=False)
+        url2 = info["formats"][0]["url"]
+        source = await discord.FFmpegOpusAudio.from_probe(
+            url2, **FFMPEG_OPTIONS)
+        voice_client.play(source)
+
+
+@client.command(aliases=["HELP", "h", "H"])
+async def help(ctx):
+    await ctx.channel.send("No")
+
+
+@client.command(aliases=["summon", "SUMMON", "j", "J", "JOIN"])
+async def join(ctx):
+    if ctx.author.voice is None:
+        await ctx.send("You aren't in a voice channel")
+    channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+        await channel.connect()
+    else:
+        await ctx.voice_client.move_to(channel)
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(QUEUE[0][1], download=False)
+            url2 = info["formats"][0]["url"]
+            source = await discord.FFmpegOpusAudio.from_probe(
+                url2, **FFMPEG_OPTIONS)
+            voice_client = ctx.voice_client
+            voice_client.play(
+                source, after=lambda x=None: asyncio.run(checkQueue(ctx)))
+
+
 client.run(TOKEN)
