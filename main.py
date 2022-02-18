3dfdb164d6212be917a2d75e30a51642de3b6a5c
Xriss Xross <xriss_xross@outlook.com>
2022-02-18 11:41:01 +0000

    play command

--- main.py
+++ main.py
@@ -59,6 +59,63 @@ async def disconnect(ctx):
         await ctx.send("I'm not currently in a voice channel")
 
 
+@client.command(aliases=["p", "P", "PLAY"])
+async def play(ctx, *args):
+    if ctx.author is None:
+        await ctx.send("You aren't in a voice channel")
+    channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+        await channel.connect()
+    else:
+        await ctx.voice_client.move_to(channel)
+
+    if args[0].startswith("https://www.youtube.com/watch?v="):
+        searchQuery = VideosSearch(args[0], limit=1)
+        songName = searchQuery.result()["result"][0]["title"]
+        songLink = "https://www.youtube.com/watch?v=" + searchQuery.result(
+        )["result"][0]["id"]
+        voice_client = ctx.voice_client
+        QUEUE.append([songName, args[0]])
+        if ctx.voice_client.is_playing():
+            pass
+        else:
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(QUEUE[0][1], download=False)
+                url2 = info["formats"][0]["url"]
+                source = await discord.FFmpegOpusAudio.from_probe(
+                    url2, **FFMPEG_OPTIONS)
+                voice_client.play(
+                    source, after=lambda x=None: asyncio.run(checkQueue(ctx)))
+                embed = discord.Embed(color=0xffff00)
+                embed.add_field(name=f"Added to queue: {songName}",
+                                value=songLink,
+                                inline=False)
+                await ctx.send(embed=embed)
+
+    else:
+        searchQueryArgs = " ".join(args)
+        searchQuery = VideosSearch(searchQueryArgs, limit=1)
+        songName = searchQuery.result()["result"][0]["title"]
+        songLink = "https://www.youtube.com/watch?v=" + searchQuery.result(
+        )["result"][0]["id"]
+        voice_client = ctx.voice_client
+        QUEUE.append([songName, songLink])
+        if ctx.voice_client.is_playing():
+            pass
+        else:
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(QUEUE[0][1], download=False)
+                url2 = info["formats"][0]["url"]
+                source = await discord.FFmpegOpusAudio.from_probe(
+                    url2, **FFMPEG_OPTIONS)
+                voice_client.play(source)
+                embed = discord.Embed(color=0xffff00)
+                embed.add_field(name=f"Added to queue: {songName}",
+                                value=songLink,
+                                inline=False)
+                await ctx.send(embed=embed)
+
+
 @client.command(aliases=["q", "Q", "QUEUE"])
 async def queue(ctx):
     if len(QUEUE) == 0:
